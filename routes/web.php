a4b71fb45d499313c455d6cdcaeaee4a08a7136e
hamza <hamzakhalem97@gmail.com>
2023-11-30 21:04:41 +0100

    seeder and fake data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +31,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin/dashboard/', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+Route::get('/agent/dashboard/', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
